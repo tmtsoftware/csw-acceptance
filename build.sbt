3de994b5d99020625095cc86c357c466652d28f0
Pritam Kadam <phkadam2008@gmail.com>
2018-02-28 11:39:24 +0530

    #357: Pritam : Extract test runner to separate project and reuse same in rest of the projects.

--- build.sbt
+++ build.sbt
@@ -19,12 +19,14 @@ lazy val `csw-acceptance` = project
 
 lazy val `location` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.Location
   )
 
 lazy val `location-agent` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.LocationAgent
   )
@@ -32,54 +34,69 @@ lazy val `location-agent` = project
 
 lazy val `config-server` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.ConfigServer
   )
 
 lazy val `config-api` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.ConfigApi
   )
 
 lazy val `config-client` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.ConfigClient
   )
 
 lazy val `config-client-cli` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.ConfigClientCli
   )
 
 lazy val `logging` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.Logging
   )
 
 lazy val `cluster-seed` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.ClusterSeed
   )
 
 lazy val `framework` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.Framework
   )
 
 lazy val `messages` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.Messages
   )
 
 lazy val `command` = project
   .enablePlugins(DeployApp)
+  .dependsOn(`csw-runner`)
   .settings(
     libraryDependencies ++= Dependencies.Command
+  )
+
+lazy val `csw-runner` = project
+  .enablePlugins(DeployApp)
+  .settings(
+    libraryDependencies ++= Dependencies.CswRunner
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,6 @@ object Dependencies {
   )
 
   val ClusterSeed: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-cluster-seed-tests`,
     CSW.`csw-cluster-seed`,
     CSW.`csw-config-server-tests`,
@@ -37,64 +36,55 @@ object Dependencies {
   ) ++ TestDependencies ++ AllCswLibs
 
   val Location: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-location-tests`,
     CSW.`csw-location`
   ) ++ TestDependencies ++ AllCswLibs
 
   val LocationAgent: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-location-agent-tests`,
     CSW.`csw-location-agent`
   ) ++ TestDependencies ++ AllCswLibs
 
   val ConfigServer: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-config-server-tests`,
     CSW.`csw-config-server`
   ) ++ TestDependencies ++ AllCswLibs
 
   val ConfigApi: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-config-api-tests`,
     CSW.`csw-config-api`
   ) ++ TestDependencies ++ AllCswLibs
 
   val ConfigClient: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-config-client-tests`,
     CSW.`csw-config-client`,
     CSW.`csw-config-server-tests`
   ) ++ TestDependencies ++ AllCswLibs
 
   val ConfigClientCli: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-config-client-cli-tests`,
     CSW.`csw-config-client-cli`
   ) ++ TestDependencies ++ AllCswLibs
 
   val Logging: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-logging-tests`,
     CSW.`csw-logging`
   ) ++ TestDependencies ++ AllCswLibs
 
   val Framework: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-framework-tests`,
     CSW.`csw-framework`
   ) ++ TestDependencies ++ AllCswLibs
 
   val Messages: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-messages-tests`,
-    CSW.`csw-messages`,
-  ) ++ TestDependencies ++ AllCswLibs
+    CSW.`csw-messages`
+  ) ++ AllCswLibs
 
   val Command: Seq[ModuleID] = Seq(
-    Libs.`scalatest`,
     CSW.`csw-command-tests`,
     CSW.`csw-command`
   ) ++ TestDependencies ++ AllCswLibs
 
+  val CswRunner: Seq[ModuleID] = Seq(Libs.`scalatest`)
 }

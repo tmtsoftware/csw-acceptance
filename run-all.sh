85122d9ee26eea0e5ce0bcec7869d760f7acb7d9
abrighton <78px3WT5s>
2019-06-13 17:07:20 +0200

    Changed run-all.sh to not exit on the first failure, so that all test-reports.txt files are generated.

--- run-all.sh
+++ run-all.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
+# Exit status
+e=0
 
-sbt admin-server/run
-sbt location-server/run
-sbt location-agent/run
-sbt config-server/run
-sbt config-api/run
-sbt config-client/run
-sbt config-cli/run
-sbt logging/run
-sbt framework/run
-sbt params/run
-sbt command-client/run
-sbt event-client/run
-sbt event-cli/run
-sbt alarm-api/run
-sbt alarm-client/run
-sbt alarm-cli/run
-sbt database/run
-sbt aas/run
-sbt time/run
+# Run all builds and exit with non-zero if any of them failed
+sbt admin-server/run || e=$?
+sbt location-server/run || e=$?
+sbt location-agent/run || e=$?
+sbt config-server/run || e=$?
+sbt config-api/run || e=$?
+sbt config-client/run || e=$?
+sbt config-cli/run || e=$?
+sbt logging/run || e=$?
+sbt framework/run || e=$?
+sbt params/run || e=$?
+sbt command-client/run || e=$?
+sbt event-client/run || e=$?
+sbt event-cli/run || e=$?
+sbt alarm-api/run || e=$?
+sbt alarm-client/run || e=$?
+sbt alarm-cli/run || e=$?
+sbt database/run || e=$?
+sbt aas/run || e=$?
+sbt time/run || e=$?
+
+exit $e

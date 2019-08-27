e15f31c800ffcf320b6adfb8a779ffc4b7b98577
Pritam Kadam <kpritam@thoughtworks.com>
2019-08-27 15:12:08 +0530

    Add missing projects in run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -11,10 +11,13 @@ sbt command/run || e=$?
 sbt config/run || e=$?
 sbt database/run || e=$?
 sbt event/run || e=$?
+sbt examples/run || e=$?
 sbt framework/run || e=$?
 sbt location/run || e=$?
 sbt logging/run || e=$?
+sbt network-utils/run || e=$?
 sbt params/run || e=$?
+sbt testkit/run || e=$?
 sbt time/run || e=$?
 
 exit $e
